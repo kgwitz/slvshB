c8b689d2a82b62276f0ba1d64107b33843ee7b91
Kurtis Gassewitz <104912874+kgwitz@users.noreply.github.com>
2023-03-24 17:55:12 -0700

    updated port

--- server/index.js
+++ server/index.js
@@ -3,12 +3,18 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const app = express()
 
+const PORT = process.env.PORT || 3030;
+
+// your code
+
+
+
 
 const userRouter = require('./controllers/user')
 const matchupRouter = require('./controllers/matchup')
 
 app.use(express.json())
-app.use(cors())
+app.use(cors('https://slvsh-bracket.onrender.com'))
 
 app.use(userRouter)
 app.use(matchupRouter)
@@ -19,6 +25,10 @@ mongoose.connect('mongodb+srv://kurtisgassewitz:soDvbabtOaNpcTdp@slvshbracket.aw
     }
 )
 
-app.listen(3001, () => {
-    console.log('SERVER RUNNING ON PORT 3001...')
-})
+// app.listen(3001, () => {
+//     console.log('SERVER RUNNING ON PORT 3001...')
+// })
+
+app.listen(PORT, () => {
+    console.log(`server started on port ${PORT}`);
+  });
